1bbe8288394a7a87ab369f2e5d23edfbabf75b59
Bruno Sam <verdibianco@hotmail.com>
2020-03-15 13:55:38 -0300

    Atualização codigo.

--- src/application/Program.java
+++ src/application/Program.java
@@ -27,6 +27,7 @@ public class Program {
 			System.out.println("Reservation: " + reservation);
 			
 			System.out.println();
+			
 			System.out.println("Enter data to update the reservation:");
 			System.out.print("Check-in date (dd/MM/yyyy): ");
 			checkIn = sdf.parse(sc.next());
